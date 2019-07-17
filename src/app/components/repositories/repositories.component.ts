ee0ff62a9f65d9e7ce86547af3b1242ba3d12283
adrian_salvador <adrian.salvador.silva@hotmail.com>
2019-07-17 12:03:20 -0500

    =Estructura base repositorios

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -10,11 +10,20 @@ export class RepositoriesComponent implements OnInit {
   repositories: any;
   repositoriesToShow: any;
 
+  languages: any = [];
+  selectedLanguages: any = [];
+
   constructor(public githubService: GithubService) {
     this.githubService.getRepositories().subscribe(repositoriesResponse => {
       this.repositories = repositoriesResponse;
+      this.repositories.forEach(repository => {
+        if (!this.languages.includes(repository.language) && !!repository.language) {
+          this.languages.push(repository.language);
+        }
+      });
       this.repositoriesToShow = [...this.repositories];
       console.log(this.repositoriesToShow);
+      console.log(this.languages);
     });
   }
 
