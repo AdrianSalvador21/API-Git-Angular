22db442cba9afc588602f87e3862ae7dc94ea940
adrian_salvador <adrian.salvador.silva@hotmail.com>
2019-07-17 17:05:08 -0500

    =Filtro repositorios

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -9,8 +9,8 @@ import {GithubService} from '../../shared/github.service';
 export class RepositoriesComponent implements OnInit {
   repositories: any;
   repositoriesToShow: any;
-
   languages: any = [];
+  languagesToShow: any = [];
   selectedLanguages: any = [];
 
   constructor(public githubService: GithubService) {
@@ -19,6 +19,7 @@ export class RepositoriesComponent implements OnInit {
       this.repositories.forEach(repository => {
         if (!this.languages.includes(repository.language) && !!repository.language) {
           this.languages.push(repository.language);
+          this.languagesToShow = [...this.languages];
         }
       });
       this.repositoriesToShow = [...this.repositories];
@@ -30,8 +31,32 @@ export class RepositoriesComponent implements OnInit {
   ngOnInit() {
   }
 
-  change(event) {
-    console.log(event);
+  change() {
+    this.selectedLanguages = [];
+    const checks = document.getElementsByClassName('lenguages');
+    let uncheckedElements = 0;
+    // @ts-ignore
+    for (let item of checks) {
+      if (!item.checked) {
+        uncheckedElements += 1;
+      } else if (!this.selectedLanguages.includes(item.id)) {
+        this.selectedLanguages.push(item.id);
+      }
+    }
+
+    if (this.selectedLanguages.length !== 0) {
+      this.filterData();
+    } else {
+      this.repositoriesToShow = [...this.repositories];
+    }
+
   }
 
+
+  filterData() {
+    this.repositoriesToShow = this.repositories.filter((repo) => {
+        return this.selectedLanguages.includes(repo.language);
+      }
+    );
+  }
 }
